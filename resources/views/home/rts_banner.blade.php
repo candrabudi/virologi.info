2c5f141119663a2a0fb7f6bf9e8c5e6f6bcde379
candrabudi <bagus.candrabudi@gmail.com>
2025-12-27 10:21:17 +0700

    update banner

--- resources/views/home/rts_banner.blade.php
+++ resources/views/home/rts_banner.blade.php
@@ -64,7 +64,7 @@
                             alt=""
                             loading="lazy"
                             decoding="async"
-                            width="25"
+                            width="65"
                         >
                     </p>
 
